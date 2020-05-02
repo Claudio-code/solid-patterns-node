d187cac5dace9c44bd904ae397a0e5a8c7af235f
claudio-code <suporte2424@outlook.com>
2020-05-02 16:21:41 -0300

    criada rotas  do agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import AppointmentController from '../controllers/AppointmentController';
+
 const appointmentsRoutes = Router();
+
+appointmentsRoutes.get('/', AppointmentController.index);
+appointmentsRoutes.post('/', AppointmentController.create);
 
 export default appointmentsRoutes;
